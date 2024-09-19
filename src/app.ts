f4a69b4deead94e740021b91bffe2dc6cf3e43ce
RonalKSP <ronalksp@gmail.com>
2024-09-19 18:04:58 -0400

    segundo

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { createBot, createFlow,MemoryDB, createProvider, addKeyword } from '@bot-whatsapp/bot'
 import {BaileysProvider, handleCtx} from '@bot-whatsapp/provider-baileys'
 
-const flowBienvenida = addKeyword('Hola').addAnswer('Buenas amig@!! Bienvenido')
+const flowBienvenida = addKeyword('Hola').addAnswer('¡Hola! ¿En que te podemos Ayudar?')
 
 /**
  * 
@@ -12,19 +12,20 @@ const main = async () => {
 
     provider.initHttpServer(3002)
 
-    provider.http?.server.post('/send-message', handleCtx(async (bot,req, res) => {
+    provider.http?.server.post('/enviar', handleCtx(async (bot,req, res) => {
         //const phone = req.body.phone
         //await bot.sendMessage('5493865537785','mensaje!',{})
         const body = req.body
         const mensaje = body.mensaje
         const numero = body.numero
-        console.log(body)
+        //console.log(body)
         await bot.sendMessage(numero,mensaje,{})
-        res.end('esto es del server de polka')
+        res.end('Respuesta del servidor de mensajes')
     }))
 
     await createBot({
-        flow: createFlow([flowBienvenida]),
+        //flow: createFlow([flowBienvenida]),
+        flow: createFlow([]),
         database: new MemoryDB(),
         provider
     })
